af73cf9f725f3e907a48ce7ba7e45c3ad054c1a3
Steffen Klempau <Steffen.Klempau@Fielmann.com>
2021-05-14 14:24:34 +0200

    init

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-# Welcome to your CDK TypeScript project!
+#  CDK TypeScript project for a notes webservice
 
-This is a blank project for TypeScript development with CDK.
+## How to:
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+- there are two lambda functions placed in the ```src/``` folder
+- all stacks are described in ```lib/serverless-ws-stack.ts```
+- aws account is determined by the cli/environment variables, so make sure you set them properly to any kind of playground account
+- the `cdk.json` file tells the CDK Toolkit how to execute your app.
 
 ## Useful commands
 
  * `npm run build`   compile typescript to js
  * `npm run watch`   watch for changes and compile
  * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+ * `npx cdk deploy`      deploy this stack to your default AWS account/region
+ * `npx cdk diff`        compare deployed stack with current state
+ * `npx cdk synth`       emits the synthesized CloudFormation template
+
+## Architecture
+
+![](/docs/arch.png)
+
+### API routes
+- get notes: ```curl https://XXXXXX.execute-api.eu-central-1.amazonaws.com/notes```
+- put notes: ```curl -X POST https://XXXXXX.execute-api.eu-central-1.amazonaws.com/notes --data '{ "title": "Hello World", "content": "abc" }' -H 'Content-Type: application/json' -i```
+- wanna try out load? try ```hey -n 1000 -m GET https://XXXXXX.execute-api.eu-central-1.amazonaws.com/notes```  
+
+## Links
+
+- DOCS: https://fielmann-ag.github.io/serverless-workshop/#/rest-api?id=bootstrapping-aws-cdk
+- AWS CDK Kickstart: https://docs.aws.amazon.com/cdk/latest/guide/hello_world.html
--- lib/serverless-ws-stack.ts
+++ lib/serverless-ws-stack.ts
@@ -1,9 +1,77 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from "@aws-cdk/aws-lambda-nodejs";
+import * as apigateway from "@aws-cdk/aws-apigatewayv2";
+import * as apigatewayIntegrations from "@aws-cdk/aws-apigatewayv2-integrations";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+
 
 export class ServerlessWsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create a dynamo db table 
+    const notesTable = new dynamodb.Table(this, "NotesTable", {
+      partitionKey: { name: "id", type: dynamodb.AttributeType.STRING },
+    })
+
+    // add a lambda function and refer to it in the src folder
+    const putNote = new lambda.NodejsFunction(this, "PutNote", {
+      entry: "src/putNote.ts",
+      handler: "handler",
+      environment: {
+        TABLE_NAME: notesTable.tableName,
+      }
+    });
+
+    // grant putnote object permissions
+    notesTable.grant(putNote, "dynamodb:PutItem");
+
+
+    // integrate putNote lambda function object with apigateway
+    const putNoteIntegration = new apigatewayIntegrations.LambdaProxyIntegration(
+      { handler: putNote }
+    );
+
+    // same for get notes 
+
+    // add a lambda function and refer to it in the src folder
+    const getNotes = new lambda.NodejsFunction(this, "GetNotes", {
+      entry: "src/getNotes.ts",
+      handler: "handler",
+      environment: {
+        TABLE_NAME: notesTable.tableName,
+      }
+    });
+
+    // grant getnote scan permissions
+    notesTable.grant(getNotes, "dynamodb:Scan");
+
+
+    // integrate getNote lambda function object with apigateway
+    const getNotesIntegration = new apigatewayIntegrations.LambdaProxyIntegration(
+      { handler: getNotes }
+    );
+
+    // create an api gateway
+    const httpApi = new apigateway.HttpApi(this, 'HttpApi');
+
+    // add route to api gateway that routes post and get request to the lambda integration
+    httpApi.addRoutes({
+      path: "/notes",
+      methods: [apigateway.HttpMethod.POST],
+      integration: putNoteIntegration
+    });
+
+    httpApi.addRoutes({
+      path: "/notes",
+      methods: [apigateway.HttpMethod.GET],
+      integration: getNotesIntegration
+    });
+
+    //  output the callable url
+    new cdk.CfnOutput(this, "URL", { value: httpApi.apiEndpoint });
+
+
+
   }
 }
